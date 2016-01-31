1be2f61b89cfe835a959d39e20e97cfb98abee86
Francesco <flanni@bnl.gov>
2016-01-31 21:22:03 +0100

    Update g4py.rb

--- g4py.rb
+++ g4py.rb
@@ -20,7 +20,7 @@
     system "export GEANT4_INSTALL=/usr/local/Cellar/geant4/4.10.02/"
     mkdir "g4py-build" do 
      system "pwd"
-     system "cmake", "-DGEANT4_INSTALL=/usr/local/Cellar/geant4/4.10.02/ ../environments/g4py" 
+     system "cmake", "../environments/g4py -DGEANT4_INSTALL=/usr/local/Cellar/geant4/4.10.02/ " 
      system "make", "install GEANT4_INSTALL=/usr/local/Cellar/geant4/4.10.02/"
     end 
   end 
