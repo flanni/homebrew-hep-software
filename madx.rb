a22d4082c20545b77e12bf635f050d70be72edba
Francesco <flanni@bnl.gov>
2016-02-21 09:31:57 +0100

    Update madx.rb

--- madx.rb
+++ madx.rb
@@ -2,24 +2,21 @@ require 'formula'
  
 class MadX < Formula
 
- desc "Mad-X"
- homepage "http://madx.web.cern.ch/madx/"
- url "http://madx.web.cern.ch/madx/releases/5.02.08/madx-src.tgz"
- version "5.02.08"
- sha256 "a264d30322c37be7ca80f66c3bd4d85147d935d0d286dcebb3609a444a82a580"
+  desc "Mad-X"
+  homepage "http://madx.web.cern.ch/madx/"
+  url "http://madx.web.cern.ch/madx/releases/5.02.08/madx-src.tgz"
+  version "5.02.08"
+  sha256 "a264d30322c37be7ca80f66c3bd4d85147d935d0d286dcebb3609a444a82a580"
 
- head "http://svn.cern.ch/guest/madx/trunk/madX", :using => :svn
+  head "http://svn.cern.ch/guest/madx/trunk/madX", :using => :svn
  
- depends_on "cmake" => :build
- depends_on "python" => :build
+  depends_on "cmake" => :build
+  depends_on "python" => :build
  
- def install
-  mkdir "build" do 
+  def install
+   mkdir "build" do 
    system "pwd"
-   args = %W[
-    ..
-   ]
-   system "cmake", *args
+   system "cmake", ".."
    system "make", "install"
    prefix.install "doc"
 
