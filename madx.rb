030a7f0fde49a2d81bdfe3fa04fa78d62e6e5e3b
Francesco <flanni@bnl.gov>
2016-02-21 11:36:20 +0100

    Update madx.rb

--- madx.rb
+++ madx.rb
@@ -17,7 +17,7 @@ class Madx < Formula
     mkdir "build" do 
       system "pwd"
       system "cmake", ".."
-      system "export", "PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:$PATH; make"
+      system "export", "PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin"
       system "make", "install"
       prefix.install "doc"
     end 
