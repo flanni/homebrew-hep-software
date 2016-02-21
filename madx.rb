6b6230b25324d94c017a24a556abf9e064d65f3f
Francesco <flanni@bnl.gov>
2016-02-21 08:56:42 +0100

    Update madx.rb

--- madx.rb
+++ madx.rb
@@ -1,31 +1,27 @@
- require 'formula'
+require 'formula'
  
- class MadX < Formula
-  desc "Mad-X"
-  homepage "http://madx.web.cern.ch/madx/"
-  url "http://madx.web.cern.ch/madx/releases/5.02.08/madx-src.tgz"
-  version "5.02.08"
-  sha256 "a264d30322c37be7ca80f66c3bd4d85147d935d0d286dcebb3609a444a82a580"
-  head "http://svn.cern.ch/guest/madx/trunk/madX", :using => :svn
-  
-  depends_on "cmake" => :build
-  depends_on "python" => :build
-  
-   def install
-    mkdir "build" do 
-     system "pwd"
-     args = %W[
-      ..
-    ]
-    system "cmake", *args
-    
-    margs = %W[
-      install
-     ]
-     system "make", *margs
+class MadX < Formula
+ desc "Mad-X"
+ homepage "http://madx.web.cern.ch/madx/"
+ url "http://madx.web.cern.ch/madx/releases/5.02.08/madx-src.tgz"
+ version "5.02.08"
+ sha256 "a264d30322c37be7ca80f66c3bd4d85147d935d0d286dcebb3609a444a82a580"
+ head "http://svn.cern.ch/guest/madx/trunk/madX", :using => :svn
  
-     end 
+ depends_on "cmake" => :build
+ depends_on "python" => :build
+ 
+ def install
+  mkdir "build" do 
+   system "pwd"
+   args = %W[
+    ..
+   ]
+   system "cmake", *args
+   system "make", "install"
+
   end 
+ end 
 end
 
 def caveats
