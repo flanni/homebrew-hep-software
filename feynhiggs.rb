cec25238c39b1f27b265ee9a95143b00cd9082b3
Francesco <flanni@bnl.gov>
2016-03-23 11:05:06 +0100

    Added files via upload

---
+++ feynhiggs.rb
@@ -0,0 +1,28 @@
+require 'formula'
+
+class Feynhiggs < Formula
+  
+  desc "Feynhiggs 2.11.3"
+  homepage "http://wwwth.mpp.mpg.de/members/heinemey/feynhiggs/cFeynHiggs.html"
+  url "http://wwwth.mpp.mpg.de/members/heinemey/feynhiggs/newversion/FeynHiggs-2.11.3.tar.gz"
+  version "2.11.3"
+  sha256 "570746c34efbbfba45b39ea7110b0069354747138ad149204f09819548b87f6a"
+
+
+
+  def install
+    args = %W[
+      --64
+      --prefix=#{prefix}
+      --enable-slhapara
+      --enable-full-g-2
+    ]
+
+    system "./configure", *args
+    system "make", "install"
+
+    prefix.install 'example'
+
+  end
+
+end
