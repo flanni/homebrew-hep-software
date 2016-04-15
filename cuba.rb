d7ee8c931993dc59f0f9dff25f44765d415efee9
Francesco <flanni@bnl.gov>
2016-04-15 22:42:55 +0200

    Added files via upload

---
+++ cuba.rb
@@ -0,0 +1,21 @@
+class Cuba < Formula
+  homepage 'http://www.feynarts.de/cuba'
+  url 'http://www.feynarts.de/cuba/Cuba-4.2.tar.gz'
+  sha256 '6b75bb8146ae6fb7da8ebb72ab7502ecd73920efc3ff77a69a656db9530a5eef'
+
+  depends_on :fortran
+
+  def install
+        args = %W[
+      --64
+      --prefix=#{prefix}
+    ]
+
+        system "./configure", *args
+        system "make"
+        system "make", "install"
+
+        prefix.install 'demo'
+  end
+
+end
