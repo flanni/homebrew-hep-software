26f66dfc0dadaad5128ee99d3f3efc461a6cf0ab
Francesco <flanni@bnl.gov>
2019-02-19 14:02:50 +0100

    Update cuba.rb

--- cuba.rb
+++ cuba.rb
@@ -3,7 +3,7 @@ class Cuba < Formula
   url 'http://www.feynarts.de/cuba/Cuba-4.2.tar.gz'
   sha256 '6b75bb8146ae6fb7da8ebb72ab7502ecd73920efc3ff77a69a656db9530a5eef'
 
-  depends_on :fortran
+
 
   def install
         args = %W[
