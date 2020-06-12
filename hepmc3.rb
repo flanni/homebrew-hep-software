26931cadcb521fea33512b4bc6b790d44f3948c2
Francesco <flanni@bnl.gov>
2020-06-12 18:48:49 +0200

    Update hepmc3.rb

--- hepmc3.rb
+++ hepmc3.rb
@@ -1,8 +1,8 @@
 class Hepmc3 < Formula
   desc "Library is to handle  event record for Monte Carlo generators"
   homepage "https://hepmc.web.cern.ch/hepmc/"
-  url "https://hepmc.web.cern.ch/hepmc/releases/HepMC3-3.2.1.tar.gz"
-  sha256 "6e4e4bb5708af105d4bf74efc2745e6efe704e942d46a8042f7dcae37a4739fe"
+  url "https://hepmc.web.cern.ch/hepmc/releases/HepMC3-3.2.2.tar.gz"
+  sha256 "0e8cb4f78f804e38f7d29875db66f65e4c77896749d723548cc70fb7965e2d41"
 
   option "with-test", "Test during installation"
   option "with-root", "Enable root IO"
