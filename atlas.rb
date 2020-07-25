069b6a05007762c93b0d12b69d8060cdb2981c87
Francesco <flanni@bnl.gov>
2020-07-25 16:35:39 +0200

    Update atlas.rb

--- atlas.rb
+++ atlas.rb
@@ -4,7 +4,7 @@ class Atlas < Formula
   url "https://sourceforge.net/projects/math-atlas/files/Stable/3.10.3/atlas3.10.3.tar.bz2"
   sha256 "2688eb733a6c5f78a18ef32144039adcd62fabce66f2eb51dd59dde806a6d2b7"
 
-  depend on "lapack"
+  depend_on "lapack"
   
   def install
     ENV['PATH']="/usr/local/bin:/usr/bin:/bin:/sbin:/usr/sbin"
